1b9b4caa98285d17d8e8e02520dc52c999c75452
maria19988 <31484675+maria19988@users.noreply.github.com>
2018-02-08 23:15:31 +0200

    m

---
+++ Lab3/exercise1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <pthread.h>
+#define STEPS 10000000
+#define NUM_THREADS 4
+
+double PI = 0.0; 
+pthread_mutex_t lock;
+ 
+void *calculatePI(void *ignored)
+{
+	int myNum = *((int *)ignored);
+	double h = 2.0 / STEPS;
+	double partial_sum = 0.0, x;&nbsp;
+ 
+  
+    for (int i = myNum; i &lt; STEPS; i += NUM_THREADS)
+	{
+         x = -1 + (i + 0.5f) * h;
+         partial_sum += sqrt(1.0 - x*x) * h;
+    }
+	pthread_mutex_lock(&amp;lock);
+	PI += partial_sum; 
+	pthread_mutex_unlock(&amp;lock);
+	return 0;
+}
+ 
+int main(int argc, char **argv) {
+ 
+	pthread_t threads[NUM_THREADS];
+	int tNum[NUM_THREADS], i;
+	pthread_mutex_init(&amp;lock, NULL);
+	for ( i = 0; i &lt; NUM_THREADS; ++i ) 
+	{
+		tNum[i] = i;
+		pthread_create(&amp;threads[i], NULL, calculatePI, (void)&amp;tNum[i]);            
+	}
+    for ( i = 0; i &lt; NUM_THREADS; ++i ) 
+	{
+		pthread_join(threads[i], NULL);
+	}
+	PI *= 2.0;
+	printf("Computed value of Pi: %12.9f\n", PI );
+	pthread_mutex_destroy(&amp;lock);
+	return 0;
+}
